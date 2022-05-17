cbb641e4165a2f3c9c1327bf413f7919f239057a
Ryan Ross <ryan@reross.com>
2022-05-17 11:36:48 -0500

    finished mock twitter interface. integrated cypress for testing. will revist app to use Postgres instead of mongoDB for practice, as well as nestJS backend

--- client/cypress/integration/tweet.spec.js
+++ client/cypress/integration/tweet.spec.js
@@ -1,5 +1,3 @@
-/* eslint-disable testing-library/await-async-utils */
-/* eslint-disable cypress/no-unnecessary-waiting */
 describe("user form flow", () => {
   beforeEach(() => {
     cy.viewport(1600, 900);
@@ -8,31 +6,31 @@ describe("user form flow", () => {
 
   it("user posts a tweet", () => {
     // Post a tweet
-    cy.get(".compose-tweet-btn").click();
+    cy.get(".compose-tweet-btn").click({ force: true });
     cy.get('textarea[name="tweet"]').type(
-      "What happened to all that fun you were having?! Come on, lets try to enjoy this!"
+      "What happened to all that fun you were having?! Come on, lets try to enjoy this!",
+      { force: true }
     );
-    cy.wait(3000);
-    cy.get(".post-tweet-btn").click();
+    cy.get(".post-tweet-btn").click({ force: true });
   });
 
   it("user posts a second tweet", () => {
     // Post a tweet
-    cy.get(".compose-tweet-btn").click();
+    cy.get(".compose-tweet-btn").click({ force: true });
     cy.get('textarea[name="tweet"]').type(
-      "That was an Attack on Titan easter egg 🥚 😄"
+      "That was an Attack on Titan easter egg 🥚 😄",
+      { force: true }
     );
-    cy.wait(3000);
-    cy.get(".post-tweet-btn").click();
+    cy.get(".post-tweet-btn").click({ force: true });
   });
 
   it("user posts a third tweet", () => {
     // Post a tweet
-    cy.get(".compose-tweet-btn").click();
+    cy.get(".compose-tweet-btn").click({ force: true });
     cy.get('textarea[name="tweet"]').type(
-      "The Rumbling arrives on Marley 😱 https://www.youtube.com/watch?v=wT2H68kEmi8"
+      "The Rumbling arrives on Marley 😱 https://www.youtube.com/watch?v=wT2H68kEmi8",
+      { force: true }
     );
-    cy.wait(3000);
-    cy.get(".post-tweet-btn").click();
+    cy.get(".post-tweet-btn").click({ force: true });
   });
 });
